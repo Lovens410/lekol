6c3ae2b47749d213021b515e741d84c9f431625c
Lovens410 <152534631+Lovens410@users.noreply.github.com>
2023-12-03 22:34:25 -0500

    password 1
    generer passworld 1

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
-
-
-/*let shortPassword = generatePassword(8) 
-// 8 la pou paramèt length
-// Afiche: anfuwpru
-
+/*let shortPassword = generatePassword(12, true, true, true)
+// length=12, uppercase=true, digits=true, specialChars=true
+// Afiche : j2#Am7Bg*HA=
 */
 
-let shortPassword = generatePassword(8)
 
-function generatePassword(length) {
+let shortPassword = generatePassword(12, true, true, true)
+
+function generatePassword(length, upperCase, digits, specialChars){
 
 	let alfabe = "abcdefghijklmnopqrstuvwxyz"
 	let modpas = ""
-	let i = 0
 
-    while(i < length)
+	
+	if(upperCase){
+		alfabe += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	}
+		
+
+	if(digits){
+		alfabe += "0123456789"
+	}
+	
+	if(specialChars){
+		alfabe += "@#!$%^&*()_|}{}:;?/.>,<"
+	}
+
+    let i = 0
+	while(i < length)
     {
     	const jenere = Math.floor(Math.random() * alfabe.length)
     	modpas += alfabe[jenere]
     	i++
     }
-    
-	return modpas
+     
+    return modpas
+
 }
+
 console.log(shortPassword)
+	
+
+
 
